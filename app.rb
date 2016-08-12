8accc10adb0b5373d46c9874739fd2883d7d15f9
Fred <fredy799@gmail.com>
2016-08-12 13:31:57 +0530

    Save as JSON

--- app.rb
+++ app.rb
@@ -4,20 +4,16 @@ require 'net/http'
 require 'openssl'
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 get'/' do #Home Page
   "
-  <br>
-  <hr>
-  <br>
+  <br><hr><br>
   <form action='/result' method='post'> 
     <label for='txtUrl'>Enter URL :</label>
     <input type='text' name='txtUrl' id='txtUrl' required>
     <input type='submit' value='Submit Url' name='btnClick'>
-  </form>
-  <br>
-  <hr>
-  <br>
+  </form><br><hr><br>
   "
 end
 
@@ -29,15 +25,30 @@ def get_page(url)
   begin
     doc = Nokogiri::HTML(open(url))
   rescue
-     return "<h1>Oops..Could not Fetch the URL</h1>"
+    return "<h1>Oops..Could not Fetch the URL</h1>"
   end
-  links   = doc.xpath("//a").map{|link| link['href'].to_s+"<br>"}
-  headers = doc.xpath("//h1","//h2").map{|header| header.text+"<br>"}
-  return "<h1><u>Headers</u></h1><b>"+headers.join+"</b><h1><u>Links</u></h1><b>"+links.join
+  @out={}
+  @out['h1']=[]
+  @out['h2']=[]
+  @out['h3']=[]
+  @out['h4']=[]
+  @out['links']=[]
+
+  docs=doc.xpath("//a").map{|link| link['href'].to_s}
+  links=docs.select{|i| (i.start_with?('https','http'))}
+  links.each{|link| @out['links'][@out['links'].size]=link}
+
+  headers_h1= doc.xpath("//h1").map{|header| header.text}
+  headers_h1.each{|i| @out['h1'][@out['h1'].size]=i.strip}
+  
+  headers_h2= doc.xpath("//h2").map{|header| header.text}
+  headers_h2.each{|i| @out['h2'][@out['h2'].size]=i.strip}
+
+  headers_h3= doc.xpath("//h3").map{|header| header.text}
+  headers_h3.each{|i| @out['h3'][@out['h3'].size]=i.strip}
+
+  headers_h4= doc.xpath("//h4").map{|header| header.text}
+  headers_h4.each{|i| @out['h4'][@out['h4'].size]=i.strip}
+  content_type:json
+  return (JSON.pretty_generate(@out))
 end
-
-
-
-
-
-
